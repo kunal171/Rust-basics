21d09a660bd9ccd1598cbe050db3b5b0d5f02c77
mestan998 <kunal170107028@alumni.iitg.ac.in>
2022-11-17 23:02:59 +0530

    added something more

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -31,4 +31,15 @@ fn main() {
         }
 
     }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+    
 }
